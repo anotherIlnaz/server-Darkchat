4f49231c634c415bd5e9dcf4d60dde4349a24192
Ilnaz <mmmr.sh.i@gmail.com>
2022-08-18 09:23:22 +0300

    First commit

---
+++ control/authController.js
@@ -0,0 +1,24 @@
+class authController {
+   async registration(req, res) {
+      try {
+      } catch (er) {
+         console.log(er);
+      }
+   }
+
+   async login(req, res) {
+      try {
+      } catch (er) {
+         console.log(er);
+      }
+   }
+
+   async getUsers(req, res) {
+      try {
+      } catch (er) {
+         console.log(er);
+      }
+   }
+}
+
+module.exports = new authController();
---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const authRouter = require("./routes/authRouter");
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use("/auth", authRouter);
+
+const start = async () => {
+   try {
+      await mongoose.connect(
+         "mongodb+srv://qwerty:qwerty123@cluster0.g13vyae.mongodb.net/?retryWrites=true&w=majority"
+      );
+      app.listen(PORT, () =>
+         console.log(`Server has been started on port ${PORT}`)
+      );
+   } catch (er) {
+      console.log(er);
+   }
+};
+
+start();
